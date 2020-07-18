d177379910a9b1ef50940881ee11b899db8f5e8d
AstroTaka <53452427+AstroTaka@users.noreply.github.com>
2020-07-18 17:05:06 +0900

    Update formatDate.ts

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -17,6 +17,10 @@ export const convertDateToDataViewDisplayText =(dateString: string): string => {
   return dayjs(dateString).format('YYYY/MM/DD HH:mm')
 }
 
+export const convertDateToDataTableText =(dateString: string): string => {
+  return dayjs(dateString).format('MM/DD')
+}
+
 export default (dateString1: string, dateString2: string) => {
   if(dayjs(dateString1).isAfter(dayjs(dateString2))){
     return dayjs(dateString1).format('YYYY/MM/DD HH:mm')
